098db1aced4ef84986dcee8ca0f09f8fca07ae3f
Mohamed Gamal <mohamed.g.shoaib@gmail.com>
2026-02-07 03:38:27 +0200

    feat: update llms and llms-full files

--- src/app/(llms)/llms.txt/route.ts
+++ src/app/(llms)/llms.txt/route.ts
@@ -1,11 +1,12 @@
 import { SITE_INFO } from "@/config/site";
 import { getAllPosts } from "@/features/blog/data/posts";
+import { USER } from "@/features/portfolio/data/user";
 
 const allPosts = getAllPosts();
 
-const content = `# chanhdai.com
+const content = `# ${new URL(SITE_INFO.url).hostname}
 
-> A minimal, pixel-perfect dev portfolio and blog to showcase my work as a Design Engineer.
+> A minimal, pixel-perfect dev portfolio and blog to showcase my work as a ${USER.jobTitle}.
 
 - [About](${SITE_INFO.url}/about.md): A quick intro to me, my tech stack, and how to connect.
 - [Experience](${SITE_INFO.url}/experience.md): Highlights from my career and key roles I've taken on.
