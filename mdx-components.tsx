eac0a3e3874e8069bc882c11969cfb99472372fb
Mohamed Gamal <mohamed.g.shoaib@gmail.com>
2026-02-15 02:38:25 +0200

    refactor: remove old version files

--- mdx-components.tsx
+++
@@ -1,9 +0,0 @@
-import type { MDXComponents } from "mdx/types";
-import { mdxComponents } from "@/components/mdx/mdx-components";
-
-export function useMDXComponents(components: MDXComponents): MDXComponents {
-  return {
-    ...mdxComponents,
-    ...components,
-  };
-}
